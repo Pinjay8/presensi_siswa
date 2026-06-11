67476ff8625750530e0360b175da7fc1d3182b46
IvanRRiadhy <ivanrifqi1002@gmail.com>
2026-06-11 17:37:34 +0700

    feat: Added Delete Action

--- src/features/classroom/containers/classroom-table.tsx
+++ src/features/classroom/containers/classroom-table.tsx
@@ -3,10 +3,11 @@ import { BaseDataTable, useAlert } from "@/features/_global";
 import { useSchool } from "@/features/schools";
 import { useMemo, useState } from "react";
 import { ModalCreateClass } from "../components";
-import { useClassroom } from "../hooks";
+import { useClassroom, useClassroomCreation } from "../hooks";
 import { classroomColumns, classroomDataFallback } from "../utils";
 import { useProfile } from "@/features/profile";
 import ModalAssignSchedule from "../components/modalAssignSchedule";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 
 export const ClassroomTable = () => {
   const resource = useClassroom();
@@ -14,7 +15,13 @@ export const ClassroomTable = () => {
   const [classRoom, setCreateClassRoom] = useState(false);
   const [openAssignSchedule, setOpenAssignSchedule] = useState(false);
   const [selectedClass, setSelectedClass] = useState<any>(null);
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
   const alert = useAlert();
+
+  const handleOpenDeleteDialog = (classroom: any) => {
+    setSelectedClass(classroom);
+    setOpenDeleteDialog(true);
+  }
 
   const handleOpenAssignSchedule = (classroom: any) => {
     setSelectedClass(classroom);
@@ -34,12 +41,26 @@ export const ClassroomTable = () => {
           ),
         },
         onAssignSchedule: handleOpenAssignSchedule,
+        onDelete: handleOpenDeleteDialog,
       }),
     [school.data],
   );
 
   const profile = useProfile();
   const isRole = profile?.user?.role === "guru" || profile?.user?.role === "siswa" || profile?.user?.role === "orangTua";
+
+  const classDelete = useClassroomCreation();
+
+  async function handleDelete() {
+    try {
+      await classDelete.delete(Number(selectedClass?.id));
+      alert.success(lang.text("successDelete"));
+      resource.query.refetch();
+      setOpenDeleteDialog(false);
+    } catch (error) {
+      alert.error(lang.text("failedDelete"));
+    }
+  }
 
   return (
     <>
@@ -76,6 +97,20 @@ export const ClassroomTable = () => {
         searchPlaceholder={lang.text("search")}
         isLoading={resource.query.isLoading}
       />
+
+            {/* Delete Dialog */}
+            <Dialog open={openDeleteDialog} onClose={() => setOpenDeleteDialog(false)}>
+              <DialogTitle>{lang.text("delete")}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  {lang.text("deleteMessage", { context: selectedClass?.user_name })}
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => setOpenDeleteDialog(false)}>{lang.text("cancel")}</Button>
+                <Button onClick={handleDelete} disabled={classDelete.isLoading}>{lang.text("delete")}</Button>
+              </DialogActions>
+            </Dialog>
     </>
   );
 };
