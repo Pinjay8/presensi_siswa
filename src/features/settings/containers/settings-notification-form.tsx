f1e8bc4006306e291d86ff1015b4b2a49c08fd2a
Alvin Cennanda <alvincennanda@gmail.com>
2026-07-31 09:32:00 +0700

    hide siswa in setting

--- src/features/settings/containers/settings-notification-form.tsx
+++ src/features/settings/containers/settings-notification-form.tsx
@@ -24,136 +24,6 @@ export const SettingsNotificationForm = () => {
 
   const navigate = useNavigate();
   const alert = useAlert();
-
-  /*
-  const form = useForm<z.infer<typeof settingsSchema>>({
-    resolver: zodResolver(settingsSchema),
-    mode: "all",
-    values: {
-        studentAttendanceNotification: false,
-        teacherAttendanceNotification: false,
-        parentAttendanceNotification: false,
-        courseAttendanceNotification: false,
-        extracurricularAttendanceNotification: false,
-    }
-  })
-
-  return(
-    <Form {...form}>
-        <form onSubmit={()=> console.log("Form: ", form)}  className= "mb-8">
-            <div className="bg-white dark:bg-zinc-950 border border-slate-200 dark:border-zinc-800 rounded-xl shadow-sm p-6 max-w-lg">
-                <div className="grid gap-6">
-                <FormField
-                    control={form.control}
-                    name="studentAttendanceNotification"
-                    render={({field, fieldState}) => {
-                        return (
-                            <FormItem className="w-full">
-                                <div className="flex flex-row items-center justify-between w-full">
-                                    <FormLabel>{lang.text("studentAttendanceNotification")}</FormLabel>
-                                    <FormControl>
-                                        <CustomSwitch
-                                            checked={field.value}
-                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => field.onChange(e.target.checked)}
-                                        />
-                                    </FormControl>
-                                </div>
-                                <FormMessage>{fieldState.error?.message}</FormMessage>
-                            </FormItem>
-                        )
-                    }}
-                />
-                <FormField
-                    control={form.control}
-                    name="teacherAttendanceNotification"
-                    render={({field, fieldState}) => {
-                        return (
-                            <FormItem className="w-full">
-                                <div className="flex flex-row items-center justify-between w-full">
-                                    <FormLabel>{lang.text("teacherAttendanceNotification")}</FormLabel>
-                                    <FormControl>
-                                        <CustomSwitch
-                                            checked={field.value}
-                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => field.onChange(e.target.checked)}
-                                        />
-                                    </FormControl>
-                                </div>
-                                <FormMessage>{fieldState.error?.message}</FormMessage>
-                            </FormItem>
-                        )
-                    }}
-                />
-                <FormField
-                    control={form.control}
-                    name="parentAttendanceNotification"
-                    render={({field, fieldState}) => {
-                        return (
-                            <FormItem className="w-full">
-                                <div className="flex flex-row items-center justify-between w-full">
-                                    <FormLabel>{lang.text("parentAttendanceNotification")}</FormLabel>
-                                    <FormControl>
-                                        <CustomSwitch
-                                            checked={field.value}
-                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => field.onChange(e.target.checked)}
-                                        />
-                                    </FormControl>
-                                </div>
-                                <FormMessage>{fieldState.error?.message}</FormMessage>
-                            </FormItem>
-                        )
-                    }}
-                />
-                <FormField
-                    control={form.control}
-                    name="courseAttendanceNotification"
-                    render={({field, fieldState}) => {
-                        return (
-                            <FormItem className="w-full">
-                                <div className="flex flex-row items-center justify-between w-full">
-                                    <FormLabel>{lang.text("courseAttendanceNotification")}</FormLabel>
-                                    <FormControl>
-                                        <CustomSwitch
-                                            checked={field.value}
-                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => field.onChange(e.target.checked)}
-                                        />
-                                    </FormControl>
-                                </div>
-                                <FormMessage>{fieldState.error?.message}</FormMessage>
-                            </FormItem>
-                        )
-                    }}
-                />
-                <FormField
-                    control={form.control}
-                    name="extracurricularAttendanceNotification"
-                    render={({field, fieldState}) => {
-                        return (
-                            <FormItem className="w-full">
-                                <div className="flex flex-row items-center justify-between w-full">
-                                    <FormLabel>{lang.text("extracurricularAttendanceNotification")}</FormLabel>
-                                    <FormControl>
-                                        <CustomSwitch
-                                            checked={field.value}
-                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => field.onChange(e.target.checked)}
-                                        />
-                                    </FormControl>
-                                </div>
-                                <FormMessage>{fieldState.error?.message}</FormMessage>
-                            </FormItem>
-                        )
-                    }}
-                />
-                </div>
-
-                <div className="flex justify-start mt-6 pt-6 border-t border-slate-100 dark:border-zinc-800">
-                    <Button type="submit">{lang.text("save")}</Button>
-                </div>
-            </div>
-        </form>
-    </Form>
-  )
-  */
-
   const { data: settingsData, isLoading, update } = useSettings();
 
   const form = useForm<z.infer<typeof settingsSchemaNew>>({
@@ -200,7 +70,7 @@ export const SettingsNotificationForm = () => {
                   name="attendanceSiswa"
                   render={({ field, fieldState }) => (
                     <FormItem className="w-full">
-                      <div className="flex flex-row items-center justify-between w-full">
+                      {/* <div className="flex flex-row items-center justify-between w-full">
                         <FormLabel className="text-sm font-normal text-slate-600 dark:text-zinc-400">
                           {lang.text("student")}
                         </FormLabel>
@@ -213,7 +83,7 @@ export const SettingsNotificationForm = () => {
                           />
                         </FormControl>
                       </div>
-                      <FormMessage>{fieldState.error?.message}</FormMessage>
+                      <FormMessage>{fieldState.error?.message}</FormMessage> */}
                     </FormItem>
                   )}
                 />
@@ -257,7 +127,7 @@ export const SettingsNotificationForm = () => {
                   name="attendanceMapelSiswa"
                   render={({ field, fieldState }) => (
                     <FormItem className="w-full">
-                      <div className="flex flex-row items-center justify-between w-full">
+                      {/* <div className="flex flex-row items-center justify-between w-full">
                         <FormLabel className="text-sm font-normal text-slate-600 dark:text-zinc-400">
                           {lang.text("student")}
                         </FormLabel>
@@ -269,7 +139,7 @@ export const SettingsNotificationForm = () => {
                             ) => field.onChange(e.target.checked)}
                           />
                         </FormControl>
-                      </div>
+                      </div> */}
                       <FormMessage>{fieldState.error?.message}</FormMessage>
                     </FormItem>
                   )}
@@ -314,7 +184,7 @@ export const SettingsNotificationForm = () => {
                   name="ekskulSiswa"
                   render={({ field, fieldState }) => (
                     <FormItem className="w-full">
-                      <div className="flex flex-row items-center justify-between w-full">
+                      {/* <div className="flex flex-row items-center justify-between w-full">
                         <FormLabel className="text-sm font-normal text-slate-600 dark:text-zinc-400">
                           {lang.text("student")}
                         </FormLabel>
@@ -326,7 +196,7 @@ export const SettingsNotificationForm = () => {
                             ) => field.onChange(e.target.checked)}
                           />
                         </FormControl>
-                      </div>
+                      </div> */}
                       <FormMessage>{fieldState.error?.message}</FormMessage>
                     </FormItem>
                   )}
